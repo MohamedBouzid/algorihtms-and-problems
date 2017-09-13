c494ad05fe333a0b6e3978e68b291b493fc26e83
MohamedBouzid <mbouzid20@yahoo.com>
2017-09-13 17:25:55 +0200

    Add files via upload

--- dp/lcs.cpp
+++ dp/lcs.cpp
@@ -2,53 +2,39 @@
 
 using namespace std;
 
-/* Recursive Longest Common Sequence */
-int lcs(string s1, string s2, int n, int m){
-	if(n==0 || m==0)
-		return 0;
-	if(s1[n-1]==s2[m-1])
-		return lcs(s1,s2,n-1,m-1)+1;
-	else
-		return max(lcs(s1,s2,n-1,m),lcs(s1,s2,n,m-1));
+/* Longest Common Substring problem bottom up solution */
+int lcs(string s1, string s2){
 
-}
+	int l1 = s1.size();
+	int l2 = s2.size(); 
+	int dp[l1+1][l2+1];
 
+	for(int i=0;i<=l1;i++){
+		dp[i][0] = 0;
+	}
+	for(int j=0;j<=l2;j++){
+		dp[0][j] = 0;
+	}
+	int max_length = 0;
 
-/* Iterative Longest Common Sequence */
-int lcs_it(string s1, string s2, int n, int m){
-
-	int mat[n+1][m+1];
-	string s;
-	for (int i=0;i<=n;i++){
-		for(int j=0;j<=m;j++){
-				if(i==0|| j==0)
-					mat[i][j]=0;
-			  else if(s1[i]!=s2[j])
-					mat[i][j]=max(mat[i-1][j],mat[i][j-1]);
-				else{
-					s=s+s1[i];
-					mat[i][j]=mat[i-1][j-1]+1;
-				}
+	for(int i=1;i<=l1;i++){
+		for(int j=1;j<=l2;j++){
+			if(s1[i-1] == s2[j-1]){
+				dp[i][j] = dp[i-1][j-1] + 1;
+			}else
+				dp[i][j] = 0;
+			max_length = max(max_length,dp[i][j]);
 		}
 	}
-	for (int i=0;i<=n;i++){
-		for(int j=0;j<=m;j++){
-			cout<<mat[i][j]<<" ";
-		}
-		cout<<"\n";
-	}
-	cout<<s;
-	return mat[n][m];
+	return max_length;
 }
-
 
 
 int main(){
 
-	string s1="AGGTAB";
-	string s2="GXTXAYB";
-	cout<<lcs(s1,s2,6,7)<<"\n";
-	cout<<lcs_it(s1,s2,6,7)<<"\n";
-	cout<<lcs(s1,s2,6,7)<<"\n";
-	return  0;
+  string X = "lkdfjghflskghkfg";
+  string Y = "fkqskbgvjfsbklnfb";
+ 	cout<<"lcs = "<<lcs(X,Y)<<"\n";
+
+	return 0;
 }
