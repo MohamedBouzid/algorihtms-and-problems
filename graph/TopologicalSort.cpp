f4172df2adab6b5e538c28cd9c1575a520c70b14
MohamedBouzid <mbouzid20@yahoo.com>
2017-09-26 03:43:59 +0200

    topological sort

---
+++ graph/TopologicalSort.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void dfs(list<int> adj[], int v, bool visited[], stack<int> *st){
+
+	visited[v] = true;
+
+
+	list<int>::iterator it;
+	for(it=adj[v].begin();it!=adj[v].end();it++){
+		if(!visited[*it]){
+			visited[*it] = true;
+			dfs(adj,*it,visited,st);
+		}
+	}
+	st->push(v);
+}
+
+void topo_sort(list<int> adj[], int  n){
+	
+	bool * visited = new bool[n];
+	stack<int> * st = new stack<int>();
+	for(int i=0;i<n;i++)
+		visited[i] = false;
+	for(int i=0;i<n;i++){
+
+		if(!visited[i]){
+			dfs(adj,i,visited, st);
+		}
+	}
+
+	while(!st->empty()){
+		cout<<st->top()<<"   ";
+		st->pop();
+	}
+
+}
+
+int main(){
+	int n,m,x,y;
+	cin>>n>>m;
+	list<int> adj[n];
+	for(int i=0;i<n;i++){
+		cin>>x>>y;
+		adj[x].push_back(y);
+	}
+
+	topo_sort(adj,n);
+
+	return 0;
+}
